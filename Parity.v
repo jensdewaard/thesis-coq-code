fab5960eecd31735e079614a294403abc9f03e62
Jens de Waard <jensdewaard@gmail.com>
2019-02-06 16:12:32 +0100

    Prove properties of parity_le
    Prove the reflexive and transistive properties of the parity_le
    relation. This makes the set of parities a preordered set with the
    parity_le relation.

--- Parity.v
+++ Parity.v
@@ -21,6 +21,14 @@ Inductive parity_le : parity -> parity -> Prop :=
   | lt_bottom : forall p, parity_le par_bottom p
   | lt_refl : forall p, parity_le p p.
 
+Lemma parity_le_refl : forall p, parity_le p p.
+destruct p; constructor. Qed.
+
+Lemma parity_le_trans : forall p1 p2 p3,
+  parity_le p1 p2 -> parity_le p2 p3 -> parity_le p1 p3.
+destruct p1, p2, p3; intros; try constructor; 
+  try inversion H; try inversion H0. Qed.
+
 (** ** Operations *)
 
 Definition parity_plus (p q : parity) : parity :=
