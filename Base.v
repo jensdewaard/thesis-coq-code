42a95cc2288fb452d66fb0840c16e45f5ff68deb
Jens de Waard <jensdewaard@gmail.com>
2019-11-07 11:14:44 +0100

    Add more to Base
    Base now exports Utf8 and a notation for composing functions.

--- Base.v
+++ Base.v
@@ -1,4 +1,12 @@
+Require Export Utf8.
+Require Import Program.Basics.
 
 Create HintDb soundness.
 
 Ltac inv H := inversion H; subst; clear H.
+
+Notation "f '$' x" := (f x)
+  (at level 20, right associativity, only parsing).
+
+Definition id := fun {A : Type} (a : A) => a.
+Notation "f ∘ g" := (compose g f).
