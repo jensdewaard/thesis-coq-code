d4f0399609bcb63ce8237fac8f7a20cc8389c4cc
Jens de Waard <jensdewaard@gmail.com>
2019-11-07 11:15:58 +0100

    Add Functor laws to the Functor class.

--- Classes/Functor.v
+++ Classes/Functor.v
@@ -1,3 +1,12 @@
-Class Functor (F : Type -> Type) (A B : Type) : Type := {
-  fmap : (A -> B) -> F A -> F B;
+Require Export Base.
+
+Class Functor (F : Type -> Type) : Type := {
+  fmap : forall {A B}, (A -> B) -> F A -> F B;
+  fmap_id : forall A, fmap (@id A) = id;
+  fmap_compose : 
+    forall (A B C : Type) (f : A -> B) (g : B -> C),
+    fmap (f ∘ g) = fmap f ∘ fmap g;
 }.
+
+Notation "x '<$>' y" := (fmap x y)
+  (at level 20, y at level 100, only parsing).
