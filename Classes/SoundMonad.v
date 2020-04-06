7f3378292a0574ce307db779159767ef8bfba87f
Jens de Waard <jensdewaard@gmail.com>
2020-04-06 13:30:21 +0200

    Removed soundmonad

--- Classes/SoundMonad.v
+++
@@ -1,7 +0,0 @@
-Require Export Base.
-Require Import Classes.PreorderedSet.
-Require Import Classes.Galois.
-Require Import Classes.Monad.
-Require Import Classes.Monad.MonadFail.
-Require Import Instances.Galois.
-
