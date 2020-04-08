883bf44bfc4f638910edc7e886d0db1a6b163380
Jens de Waard <jensdewaard@gmail.com>
2020-04-08 12:45:47 +0200

    Add definition of sound monadfail

--- Classes/Monad/MonadFail.v
+++ Classes/Monad/MonadFail.v
@@ -1,5 +1,5 @@
 Require Export Base.
-Require Import Classes.Monad.
+Require Import Classes.Monad Classes.Galois.
 
 Class MonadFail M {MM : Monad M} : Type := {
   fail : ∀ {A : Type}, M A;
@@ -7,3 +7,10 @@ Class MonadFail M {MM : Monad M} : Type := {
 }.
 Arguments fail : simpl never.
 Hint Rewrite @fail_left : monads.
+
+Class MonadFail_sound (M M' : Type → Type) 
+  {GM : ∀ A A', Galois A A' → Galois (M A) (M' A')}
+  {MM : Monad M} {MM' : Monad M'}
+  {MF : MonadFail M} {MF' : MonadFail M'} : Prop :=
+  fail_sound : ∀ {A A' : Type} {GA : Galois A A'} (m' : M' A'),
+    γ (fail (A:=A) (M:=M)) m'.
