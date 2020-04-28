5c53f76c23bdc688c7a99143179ebc94d86ee8ab
Jens de Waard <jensdewaard@gmail.com>
2020-04-28 15:18:59 +0200

    Loosen monad constrait on MonadState.
    The MonadState interface no longer requires the underlying type M to be
    a monad.

--- Classes/Monad/MonadState.v
+++ Classes/Monad/MonadState.v
@@ -2,19 +2,18 @@ Require Export Base Coq.Strings.String.
 Require Import Classes.Monad.
 Require Import Classes.Galois.
 
-Class MonadState (S : Type) (M : Type -> Type) `{MM : Monad M} :=
+Class MonadState (S : Type) (M : Type -> Type) :=
 {
   get : M S;
   put : S -> M unit;
 }.
 
-Class get_state_sound (M M' : Type → Type) `{MM : Monad M} `{MM' : Monad M'}
+Class get_state_sound (M M' : Type → Type) 
   {GM : ∀ A A' : Type, Galois A A' → Galois (M A) (M' A')}
   {S S' : Type} {GS : Galois S S'}  {MS : MonadState S M}
   {MS' : MonadState S' M'} : Prop := get_sound : γ get get.
 
 Class put_state_sound (M M' : Type → Type) 
-  `{MM : Monad M} `{MM' : Monad M'}
   {GM : ∀ A A' : Type, Galois A A' → Galois (M A) (M' A')}
   {S S' : Type} {GS : Galois S S'} 
   {MS : MonadState S M} {MS' : MonadState S' M'} : Prop := 
